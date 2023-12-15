fc4238c3f1f3be2b3b69df20a6cca9aebb34cfa9
Veronica Lusby <vlusby@indeed.com>
2023-12-15 15:29:30 -0600

    day13

---
+++ src/day14/Main.java
@@ -0,0 +1,14 @@
+package day14;
+
+import util.Utility;
+
+import java.io.IOException;
+import java.util.List;
+
+public class Main {
+    public static void main(final String[] args) throws IOException {
+        final List<String> inputs = Utility.readFile("day14", "input1");
+        int sum = 0;
+        System.out.println(sum);
+    }
+}
